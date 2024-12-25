00f0bc081ddeefb2029752102478a14b19ceba76
DESKTOP-H746NHR\HP <aptechsheh@gmail.com>
2024-12-25 20:55:22 +0500

    Cors policy

--- server.js
+++ server.js
@@ -7,7 +7,15 @@ var authRouter = require('./Routes/auth-router.js');
 var orderRouter = require('./Routes/order-router.js');
 var app = express();
 app.use(bodyParser.json());
-app.use(cors());
+
+app.use(cors({
+    origin: true,  // Allow all origins
+    methods: ['GET', 'POST', 'PUT', 'OPTIONS'],  
+    allowedHeaders: ['Content-Type', 'Authorization'],  
+    credentials: true  
+  }));
+  
+  app.options('*', cors()); 
 app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/order", orderRouter);
