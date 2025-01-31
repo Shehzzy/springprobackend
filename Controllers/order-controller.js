7d08f494a742dbf2eb1d12792a9cc3248771ad97
DESKTOP-H746NHR\HP <aptechsheh@gmail.com>
2025-01-31 23:23:56 +0500

    .

--- Controllers/order-controller.js
+++ Controllers/order-controller.js
@@ -388,11 +388,73 @@ const orderSubmit = async (req, res) => {
     // Log incoming data for debugging purposes
     console.log("Incoming data:", req.body);
 
+    // Initialize customer variable
+    let customer = null;
+
     // Validate customer data
-    // ... (existing customer validation logic)
+    if (customerData) {
+      customer = await customerModel.findOne({ taxid: customerData.taxid });
+
+      if (customer) {
+        // Update the existing customer only for modified fields
+        const fieldsToUpdate = {};
+        for (const field of [
+          "businesslegalname",
+          "businessaddress",
+          "businesscity",
+          "businessstate",
+          "businesszip",
+          "contactname",
+          "contactphone",
+          "contactemail",
+          "shippingaddress",
+          "shippingcity",
+          "shippingstate",
+          "shippingzip",
+        ]) {
+          if (customer[field] !== customerData[field]) {
+            fieldsToUpdate[field] = customerData[field];
+          }
+        }
+
+        if (Object.keys(fieldsToUpdate).length) {
+          customer = await customerModel.findByIdAndUpdate(
+            customer._id,
+            { $set: fieldsToUpdate },
+            { new: true }
+          );
+          console.log("Customer updated:", customer);
+        }
+      } else {
+        customer = await customerModel.create({
+          ...customerData,
+          agentId: userId,
+        });
+        console.log("Customer created:", customer);
+      }
+    }
 
     // Handle IMEI creation/referencing
-    // ... (existing IMEI handling logic)
+    const existingIMEIs = await imeiModel.find({
+      imei: { $in: imeiNumbers },
+      userId,
+    });
+    const existingIMEIIds = existingIMEIs.map((imei) => imei._id);
+    const newIMEIs = imeiNumbers.filter(
+      (imei) => !existingIMEIs.some((existing) => existing.imei === imei)
+    );
+
+    // Insert new IMEIs into the database
+    const createdIMEIs =
+      newIMEIs.length > 0
+        ? await imeiModel.insertMany(newIMEIs.map((imei) => ({ userId, imei })))
+        : [];
+    console.log("Created IMEIs:", createdIMEIs);
+
+    const allIMEIIds = [
+      ...existingIMEIIds,
+      ...createdIMEIs.map((imei) => imei._id),
+    ];
 
     // Convert shippingAddresses to a Map
     const shippingAddressesMap = new Map();
@@ -404,7 +466,7 @@ const orderSubmit = async (req, res) => {
     const order = await orderModel.create({
       ...orderData,
       userId,
-      customerId: customer ? customer._id : null,
+      customerId: customer ? customer._id : null, // Now customer is always defined
       imeiNumbers: allIMEIIds,
       carrierInfos,
       accounts: accountFields,
